25ec5eb9de5aa5804f8de22c36fa4c4b00568dd1
vzhestkov <vzhestkov@suse.com>
2026-01-02 11:26:41 +0100

    Do not try to create /etc/apt/sources.list if not Debian

--- tests/pytests/functional/states/pkgrepo/test_debian.py
+++ tests/pytests/functional/states/pkgrepo/test_debian.py
@@ -23,12 +23,12 @@ pytestmark = [
 @pytest.fixture
 def pkgrepo(states, grains):
     sources = pathlib.Path("/etc/apt/sources.list")
-    if not sources.exists():
-        sources.touch()
     if grains["os_family"] != "Debian":
         raise pytest.skip.Exception(
             "Test only for debian based platforms", _use_item_location=True
         )
+    if not sources.exists():
+        sources.touch()
     return states.pkgrepo
 
 
